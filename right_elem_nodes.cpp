e4abb64e693311962c40e02d742c46dcda083751
Aniruddha Bhattacharya <41885111+bhattacharya586@users.noreply.github.com>
2020-02-04 18:18:00 +0530

    Add files via upload

---
+++ right_elem_nodes.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+typedef struct node{
+    struct node* left;
+    int data;
+    struct node* right;
+}nodetype;
+nodetype* insert(nodetype *root,int key);
+void display(nodetype *root,int key);
+int main(){
+    vector<int> x;
+    nodetype* root=NULL;
+    cout<<"Enter total no. of elements"<<endl;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int data;
+        cin>>data;
+        x.push_back(data);
+        root = insert(root,data);
+    }
+    cout<<"Display"<<endl;
+    display(root,root->data);
+    return 0;
+}
+nodetype* insert(nodetype *root,int key){
+    if(root == NULL){
+        nodetype *p = (nodetype *)malloc(sizeof(nodetype));
+        (p)->data = key;
+        (p)->left = (p)->right = NULL;
+        root = p;
+        return root;
+    }
+    else{
+        if((root)->data>key){
+            (root)->left = insert((root)->left,key);
+        }
+        else{
+            (root)->right = insert((root)->right,key);
+        }
+    }
+}
+void display(nodetype *root,int key){
+    if (root == NULL) 
+        return; 
+    if(root->data>key){
+        cout<<root->data<<" ";
+    }
+    key=root->data;
+    display(root->left,key); 
+    display(root->right,key); 
+}
